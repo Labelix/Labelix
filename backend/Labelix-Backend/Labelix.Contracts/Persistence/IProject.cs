743bb42298e6e2c822d1a5f91286565b82c7ed85
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 16:16:23 +0200

    Finished IProject

--- backend/Labelix-Backend/Labelix.Contracts/Persistence/IProject.cs
+++ backend/Labelix-Backend/Labelix.Contracts/Persistence/IProject.cs
@@ -8,6 +8,9 @@ namespace Labelix.Contracts.Persistence
     {
         string Name { get; set; }
         string Description { get; set; }
-
+        DateTime CreationDate { get; set; }
+        bool FinishedAnnotation { get; set; }
+        IImage ListOfImages { get; set; }
+        ILabel ListOfLabel { get; set; }
     }
 }
--- backend/Labelix-Backend/Labelix.Logic/DataContext/Db/LabelixDbContext.cs
+++ backend/Labelix-Backend/Labelix.Logic/DataContext/Db/LabelixDbContext.cs
@@ -11,21 +11,6 @@ namespace Labelix.Logic.DataContext.Db
     {
         public DbSet<Project> Projects { get; set; }
 
-        //private static string ConnectionString { get; set; } = "Data Source=(localdb)\\MSSQLLocalDb;Database=LabelixDB;Integrated Security=True;";
-
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    base.OnConfiguring(optionsBuilder);
-        //    BeforeConfiguring(optionsBuilder);
-        //    optionsBuilder.UseSqlServer(ConnectionString);
-        //    AfterConfiguring(optionsBuilder);
-        //}
-        //partial void BeforeConfiguring(DbContextOptionsBuilder optionsBuilder);
-        //partial void AfterConfiguring(DbContextOptionsBuilder optionsBuilder);
-
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Host=localhost;Port=5422;Database=postgres;Username=postgres;Password=mysecretpassword");
     }
