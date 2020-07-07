f8c629bb40ecb6090cd041ccf7d8ea8b906d1ce2
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 08:45:35 +0200

    Added ICopyable

---
+++ backend/Labelix-Backend/Labelix.Contracts/ICopyable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labelix.Contracts
+{
+    public partial interface ICopyable<T>
+    {
+        void CopyProperties(T other);
+    }
+}
