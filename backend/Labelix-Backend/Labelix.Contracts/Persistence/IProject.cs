1ddfd6ddda5ea5426ccb32d63545e5258f6b7326
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 10:11:49 +0200

    Added IProject

---
+++ backend/Labelix-Backend/Labelix.Contracts/Persistence/IProject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labelix.Contracts.Persistence
+{
+    public interface IProject : IIdentifiable, ICopyable<IProject>
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+
+    }
+}
