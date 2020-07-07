e12e6be9eb10cf8b3b9af388e8c58790f572224b
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 16:59:29 +0200

    Finished Image

--- backend/Labelix-Backend/Labelix.Logic/Entities/Persistence/Image.cs
+++ backend/Labelix-Backend/Labelix.Logic/Entities/Persistence/Image.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Labelix.Contracts.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Labelix.Logic.Entities.Persistence
 {
-    class Image
+    class Image : IdentityObject, IImage
     {
+        public Image (string imagePath, string labeledPath)
+        {
+            ImagePath = imagePath;
+            LabeledPath = labeledPath;
+        }
+
+        public string ImagePath { get; set; }
+        public string LabeledPath { get; set; }
+
+        public void CopyProperties(IImage other)
+        {
+            ImagePath = other.ImagePath;
+            LabeledPath = other.LabeledPath;
+        }
     }
 }
