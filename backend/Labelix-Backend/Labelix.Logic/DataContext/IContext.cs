397af2de8505a8e488810e008b24d90ce89f70ca
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 09:25:39 +0200

    Added ControllerObject

--- backend/Labelix-Backend/Labelix.Logic/DataContext/IContext.cs
+++ backend/Labelix-Backend/Labelix.Logic/DataContext/IContext.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Labelix.Contracts;
+using Labelix.Logic.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Labelix.Logic.DataContext
 {
-    class IContext
+    internal partial interface IContext : IDisposable
     {
+        #region Async-Methods
+        Task<int> CountAsync<I, E>()
+            where I : IIdentifiable
+            where E : IdentityObject, I;
+
+        Task<E> CreateAsync<I, E>()
+            where I : IIdentifiable
+            where E : IdentityObject, ICopyable<I>, I, new();
+
+        Task<E> InsertAsync<I, E>(E entity)
+            where I : IIdentifiable
+            where E : IdentityObject, ICopyable<I>, I, new();
+
+        Task<E> UpdateAsync<I, E>(E entity)
+            where I : IIdentifiable
+            where E : IdentityObject, ICopyable<I>, I, new();
+
+        Task<E> DeleteAsync<I, E>(int id)
+            where I : IIdentifiable
+            where E : IdentityObject, I;
+
+        public Task<E> DeleteAllAsync<I, E>()
+            where I : IIdentifiable
+            where E : IdentityObject, ICopyable<I>, I, new();
+        Task SaveAsync();
+        #endregion Async-Methods
     }
 }
