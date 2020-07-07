4be1d77579849e2f1879dde9e44b6568b8630c17
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 16:20:40 +0200

    Added ILabel

---
+++ backend/Labelix-Backend/Labelix.Contracts/Persistence/ILabel.cs
@@ -0,0 +1,8 @@
+﻿namespace Labelix.Contracts.Persistence
+{
+    public interface ILabel : IIdentifiable, ICopyable<ILabel>
+    {
+        string Name { get; set; }
+        string Color { get; set; }
+    }
+}
--- backend/Labelix-Backend/Labelix.Logic/Entities/Persistence/Project.cs
+++ backend/Labelix-Backend/Labelix.Logic/Entities/Persistence/Project.cs
@@ -9,18 +9,22 @@ namespace Labelix.Logic.Entities.Persistence
     {
         public Project() { }
 
-        public Project(string name, string description)
+        public Project(string name, string description, string a)
         {
             Name = name;
             Description = description;
+            A = a;
         }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public string A { get; set; }
 
         public void CopyProperties(IProject other)
         {
             Name = other.Name;
             Description = other.Description;
+            A = other.A;
         }
     }
 }
