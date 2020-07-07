db44cd2cf092065cbf7c25f6cbfd86b847accdae
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 10:17:08 +0200

    Added CommonBase

---
+++ backend/Labelix-Backend/CommonBase/Extensions/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonBase.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static void CheckArgument(this object source, string name)
+        {
+            if (source == null)
+                throw new ArgumentNullException(name);
+        }
+    }
+}
