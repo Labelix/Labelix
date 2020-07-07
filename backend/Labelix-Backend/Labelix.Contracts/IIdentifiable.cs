ff467c58c34beff581f650b715a4d966567d7ad0
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 08:45:51 +0200

    Added IIdentifiable

---
+++ backend/Labelix-Backend/Labelix.Contracts/IIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labelix.Contracts
+{
+    public partial interface IIdentifiable
+    {
+        int Id { get; }
+    }
+}
