610fbe9f7a28e69d4db107809a6a4584f23b9079
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 16:20:31 +0200

    Added IImage

---
+++ backend/Labelix-Backend/Labelix.Contracts/Persistence/IImage.cs
@@ -0,0 +1,8 @@
+﻿namespace Labelix.Contracts.Persistence
+{
+    public interface IImage: IIdentifiable, ICopyable<IImage>
+    {
+        string ImagePath { get; set; }
+        string LabeledPath { get; set; }
+    }
+}
