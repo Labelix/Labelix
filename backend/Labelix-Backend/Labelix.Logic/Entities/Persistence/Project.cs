7fbe811cec898b675f545ffa52dc71ee0373cfb0
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-07 10:11:56 +0200

    Added Project

---
+++ backend/Labelix-Backend/Labelix.Logic/Entities/Persistence/Project.cs
@@ -0,0 +1,26 @@
+﻿using Labelix.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labelix.Logic.Entities.Persistence
+{
+    class Project : IdentityObject, IProject
+    {
+        public Project() { }
+
+        public Project(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void CopyProperties(IProject other)
+        {
+            Name = other.Name;
+            Description = other.Description;
+        }
+    }
+}
