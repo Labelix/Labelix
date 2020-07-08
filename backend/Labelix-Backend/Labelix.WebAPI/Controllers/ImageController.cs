e917c46c6961b752d4fc311dc355c96475994748
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-08 14:16:08 +0200

    Added ImageController

---
+++ backend/Labelix-Backend/Labelix.WebAPI/Controllers/ImageController.cs
@@ -0,0 +1,16 @@
+﻿using Labelix.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Contract = Labelix.Contracts.Persistence.IImage;
+using Model = Labelix.Transfer.Persistence.Image;
+namespace Labelix.WebAPI.Controllers
+{
+    public class ImageController : GenericController<Contract, Model>
+    {
+        [HttpGet("/api/Image/{id}")]
+        public Task<Model> GetAsync(int id)
+        {
+            return GetModelByIdAsync(id);
+        }
+    }
+}
