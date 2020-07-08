0b1cba02df546abedf8ea3900da1890767144266
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-08 14:17:29 +0200

    Made DBContext generic

--- backend/Labelix-Backend/Labelix.Logic/DataContext/Db/LabelixDbContext.cs
+++ backend/Labelix-Backend/Labelix.Logic/DataContext/Db/LabelixDbContext.cs
@@ -1,17 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Collections.Generic;
+﻿using Labelix.Logic.Entities.Persistence;
 using Microsoft.EntityFrameworkCore;
-using Labelix.Logic.Entities.Persistence;
 
 namespace Labelix.Logic.DataContext.Db
 {
-    partial class LabelixDbContext : DbContext
+    partial class LabelixDbContext : GenericDbContext
     {
-        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Image> ImageSet { get; set; }
+        public DbSet<Label> LabelSet { get; set; }
+        /*
+        protected DbSet<Project> ProjectSet
+        {
+            get;
+            set;
+        }
+        */
+        public override DbSet<E> Set<I, E>()
+        {
+            DbSet<E> result = null;
+            if (typeof(I) == typeof(Labelix.Contracts.Persistence.IImage))
+            {
+                result = ImageSet as DbSet<E>;
+            }
+            else if (typeof(I) == typeof(Labelix.Contracts.Persistence.ILabel))
+            {
+                result = LabelSet as DbSet<E>;
+            }
+            /*
+            else if (typeof(I) == typeof(Labelix.Contracts.Persistence.IProject))
+            {
+                result = ProjectSet as DbSet<E>;
+            }
+            */
+            return result;
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Host=localhost;Port=5422;Database=postgres;Username=postgres;Password=mysecretpassword");
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Host=localhost;Port=5422;Database=postgres;Username=postgres;Password=mysecretpassword");
+        {
+            base.OnConfiguring(optionsBuilder);
+            BeforeConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5422;Database=postgres;Username=postgres;Password=mysecretpassword");
+            AfterConfiguring(optionsBuilder);
+        }
+        partial void BeforeConfiguring(DbContextOptionsBuilder optionsBuilder);
+        partial void AfterConfiguring(DbContextOptionsBuilder optionsBuilder);
+
     }
 }
