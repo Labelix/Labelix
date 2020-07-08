4bc6c9a786519256307516c35fe7a5d7f60ed40a
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-08 17:27:48 +0200

    Added Label API Controller

--- backend/Labelix-Backend/Labelix.WebAPI/Controllers/LabelController.cs
+++ backend/Labelix-Backend/Labelix.WebAPI/Controllers/LabelController.cs
@@ -1,14 +1,18 @@
-﻿using Labelix.WebApi.Controllers;
-using Microsoft.AspNetCore.Mvc;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Contract = Labelix.Contracts.Persistence.IImage;
-using Model = Labelix.Transfer.Persistence.Image;
+using Labelix.WebApi.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Contract = Labelix.Contracts.Persistence.ILabel;
+using Model = Labelix.Transfer.Persistence.Label;
+
 namespace Labelix.WebAPI.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class ImageController : GenericController<Contract, Model>
+    public class LabelController : GenericController<Contract, Model>
     {
         [HttpGet("{id}")]
         public Task<Model> GetAsync(int id)
