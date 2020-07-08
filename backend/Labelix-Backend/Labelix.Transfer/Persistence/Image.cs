6fdf980a85edd37af9073549b2ffe8dd682ce466
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-08 14:14:23 +0200

    Added Image Transfer Object

---
+++ backend/Labelix-Backend/Labelix.Transfer/Persistence/Image.cs
@@ -0,0 +1,22 @@
+﻿using Labelix.Contracts.Persistence;
+using System.Text.Json.Serialization;
+
+namespace Labelix.Transfer.Persistence
+{
+    public partial class Image : IImage
+    {
+        [JsonPropertyName("ImagePath")]
+        public string ImagePath { get; set; } = "";
+        [JsonPropertyName("LabeledPath")]
+        public string LabeledPath { get; set; } = "";
+
+
+        public void CopyProperties(IImage other)
+        {
+            Id = other.Id;
+            ImagePath = other.ImagePath;
+            LabeledPath = other.LabeledPath;
+        }
+    }
+    partial class Image : TransferObject { }
+}
