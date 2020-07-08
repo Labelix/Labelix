efab0a58ea363e07786a761cf11ba6cba2328807
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-08 14:17:29 +0200

    Made DBContext generic

--- backend/Labelix-Backend/Labelix.Logic/Entities/IdentityObject.cs
+++ backend/Labelix-Backend/Labelix.Logic/Entities/IdentityObject.cs
@@ -1,7 +1,4 @@
 ﻿using Labelix.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Labelix.Logic.Entities
 {
--- backend/Labelix-Backend/Labelix.Logic/Factory.cs
+++ backend/Labelix-Backend/Labelix.Logic/Factory.cs
@@ -1,19 +1,53 @@
 ﻿using Labelix.Logic.DataContext.Db;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Labelix.Logic
 {
-    class Factory
+    public static partial class Factory
     {
-        public void ConfigureServices(IServiceCollection services)
+        static Factory()
         {
-            // Other DI initializations
+            ClassConstructing();
+            ClassConstructed();
+        }
+        static partial void ClassConstructing();
+        static partial void ClassConstructed();
+        public enum PersistenceType
+        {
+            Db,
+            //Csv,
+            //Ser,
+        }
+        public static PersistenceType Persistence { get; set; } = Factory.PersistenceType.Db;
+        internal static DataContext.IContext CreateContext()
+        {
+            DataContext.IContext result = null;
 
-      
+            if (Persistence == PersistenceType.Db)
+            {
+                result = new LabelixDbContext();
+            }
+            return result;
+        }
+    }
+    public static partial class Factory
+    {
+        public static Contracts.Client.IControllerAccess<I> Create<I>() where I : Contracts.IIdentifiable
+        {
+            Contracts.Client.IControllerAccess<I> result = null;
+            if (typeof(I) == typeof(Labelix.Contracts.Persistence.IImage))
+            {
+                result = new Controllers.Persistence.ImageController(CreateContext()) as Contracts.Client.IControllerAccess<I>;
+            }
+            else if (typeof(I) == typeof(Labelix.Contracts.Persistence.ILabel))
+            {
+                //result = new Controllers.Persistence.App.ExpenseController(CreateContext()) as Contracts.Client.IControllerAccess<I>;
+            }
+            else if (typeof(I) == typeof(Labelix.Contracts.Persistence.IProject))
+            {
+                //result = new Controllers.Persistence.Account.LoginSessionController(CreateContext()) as Contracts.Client.IControllerAccess<I>;
+            }
+
+            return result;
         }
     }
 }
