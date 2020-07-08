29a207507f1ca974cc6c1669de17913dfa9ddcfc
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-08 14:15:35 +0200

    Added generic Controller

--- backend/Labelix-Backend/Labelix.Logic/Controllers/GenericController.cs
+++ backend/Labelix-Backend/Labelix.Logic/Controllers/GenericController.cs
@@ -1,19 +1,28 @@
-﻿using System;
+using CommonBase.Extensions;
+using Labelix.Contracts.Client;
+using Labelix.Logic.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
 using System.Threading.Tasks;
-using CommonBase.Extensions;
-using Labelix.Contracts.Controller;
-using Labelix.Logic.DataContext;
 
 namespace Labelix.Logic.Controllers
 {
-    internal abstract partial class GenericController<E, I> : ControllerObject, IControllerAccess<I>
-       where E : Entities.IdentityObject, I, Contracts.ICopyable<I>, new()
-       where I : Contracts.IIdentifiable
+    /// <inheritdoc cref="IControllerAccess{T}"/>
+    /// <summary>
+    /// This generic class implements the base properties and operations defined in the interface. 
+    /// </summary>
+    /// <typeparam name="E">The entity type of element in the controller.</typeparam>
+    /// <typeparam name="I">The interface type which implements the entity.</typeparam>
+    internal abstract partial class GenericController<I, E> : ControllerObject, IControllerAccess<I>
+        where I : Contracts.IIdentifiable
+        where E : Entities.IdentityObject, I, Contracts.ICopyable<I>, new()
     {
-        protected abstract IEnumerable<E> Set { get; }
+        public int MaxPageSize => 500;
+
+        protected IEnumerable<E> Set() => Context.Set<I, E>();
 
         protected GenericController(IContext context)
             : base(context)
@@ -23,55 +32,79 @@ namespace Labelix.Logic.Controllers
         protected GenericController(ControllerObject controllerObject)
             : base(controllerObject)
         {
-
         }
 
         #region Async-Methods
         public Task<int> CountAsync()
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             return Context.CountAsync<I, E>();
         }
         public virtual Task<I> GetByIdAsync(int id)
         {
-            return Task.Run<I>(() =>
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
+            return Task.Run<I>(() => Set().SingleOrDefault(i => i.Id == id));
+        }
+        public async virtual Task<IEnumerable<I>> GetAllAsync()
+        {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
+            int idx = 0;
+            int qryCount = 0;
+            List<I> result = new List<I>();
+
+            do
             {
-                var result = default(E);
-                var item = Set.SingleOrDefault(i => i.Id == id);
+                var qry = await GetPageListAsync(idx++, MaxPageSize).ConfigureAwait(false);
 
-                if (item != null)
-                {
-                    result = new E();
-                    result.CopyProperties(item);
-                }
-                return result;
-            });
+                qryCount = qry.Count();
+                result.AddRange(qry);
+            } while (qryCount == MaxPageSize);
+            return result;
         }
-        internal virtual Task<IEnumerable<E>> QueryAsync(Func<E, bool> predicate)
+        public virtual Task<IEnumerable<I>> GetPageListAsync(int pageIndex, int pageSize)
         {
-            return Task.Run(() => Set.Where(predicate));
-        }
-        public virtual Task<IEnumerable<I>> GetAllAsync()
-        {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
+            if (pageSize < 1 && pageSize > MaxPageSize)
+                throw new Exception("Invalide");
+
             return Task.Run<IEnumerable<I>>(() =>
-                Set.Select(i =>
-                {
-                    var result = new E();
+                Set().Skip(pageIndex * pageSize)
+                     .Take(pageSize));
+        }
+        public virtual Task<IEnumerable<I>> QueryPageListAsync(string predicate, int pageIndex, int pageSize)
+        {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
 
-                    result.CopyProperties(i);
-                    return result;
-                }));
+            if (pageSize < 1 && pageSize > MaxPageSize)
+                throw new Exception("Invalide");
+
+            return Task.Run<IEnumerable<I>>(() =>
+                Set().AsQueryable()
+                     .Where(predicate)
+                     .Skip(pageIndex * pageSize)
+                     .Take(pageSize));
         }
         public virtual Task<I> CreateAsync()
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             return Task.Run<I>(() => new E());
         }
 
         protected virtual Task BeforeInsertingAsync(E entity)
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             return Task.FromResult(0);
         }
         public virtual Task<I> InsertAsync(I entity)
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             entity.CheckArgument(nameof(entity));
 
             var entityModel = new E();
@@ -81,21 +114,18 @@ namespace Labelix.Logic.Controllers
         }
         public virtual async Task<I> InsertAsync(E entity)
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             entity.CheckArgument(nameof(entity));
 
             await BeforeInsertingAsync(entity);
             var result = await Context.InsertAsync<I, E>(entity);
+            await AfterInsertedAsync(result);
             return result;
         }
         protected virtual Task AfterInsertedAsync(E entity)
         {
-            SaveChangesAsync();
             return Task.FromResult(0);
-        }
-
-        public virtual Task DeleteAllAsync()
-        {
-            return Context.DeleteAllAsync<I, E>();
         }
 
         protected virtual Task BeforeUpdatingAsync(E entity)
@@ -104,9 +134,11 @@ namespace Labelix.Logic.Controllers
         }
         public virtual async Task<I> UpdateAsync(I entity)
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             entity.CheckArgument(nameof(entity));
 
-            var entityModel = Set.SingleOrDefault(i => i.Id == entity.Id);
+            var entityModel = Set().SingleOrDefault(i => i.Id == entity.Id);
 
             if (entityModel != null)
             {
@@ -121,6 +153,8 @@ namespace Labelix.Logic.Controllers
         }
         public virtual async Task<I> UpdateAsync(E entity)
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             entity.CheckArgument(nameof(entity));
 
             await BeforeUpdatingAsync(entity);
@@ -139,6 +173,8 @@ namespace Labelix.Logic.Controllers
         }
         public async Task DeleteAsync(int id)
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             await BeforeDeletingAsync(id);
             var item = await Context.DeleteAsync<I, E>(id);
 
@@ -154,8 +190,28 @@ namespace Labelix.Logic.Controllers
 
         public Task SaveChangesAsync()
         {
+            CheckAuthorization(GetType(), MethodBase.GetCurrentMethod());
+
             return Context.SaveAsync();
         }
         #endregion Async-Methods
+
+        #region Internal-Methods
+        internal virtual Task<IEnumerable<E>> QueryAsync(Func<E, bool> predicate)
+        {
+            return Task.Run(() => Set().Where(predicate));
+        }
+        internal virtual Task<IEnumerable<E>> QueryAsync(string predicate, int pageIndex, int pageSize)
+        {
+            if (pageSize < 1 && pageSize > MaxPageSize)
+                throw new Exception("Invalide");
+
+            return Task.Run<IEnumerable<E>>(() =>
+                Set().AsQueryable()
+                     .Where(predicate)
+                     .Skip(pageIndex * pageSize)
+                     .Take(pageSize));
+        }
+        #endregion Internal-Methods
     }
 }
