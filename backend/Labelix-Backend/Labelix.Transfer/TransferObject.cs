5a7660821715bb6c2fe85cbbd6430accedbe08a9
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-08 14:14:45 +0200

    Added TransferObject

---
+++ backend/Labelix-Backend/Labelix.Transfer/TransferObject.cs
@@ -0,0 +1,9 @@
+﻿namespace Labelix.Transfer
+{
+    public class TransferObject : Contracts.IIdentifiable
+    {
+        public virtual int Id { get; set; }
+
+        public byte[] Timestamp { get; set; }
+    }
+}
