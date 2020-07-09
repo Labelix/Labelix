b685f37fbcfefef54209eda0778e893abacbfcb6
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-09 17:42:38 +0200

    Add AIConfigController

---
+++ ai-backend/Labelix-AI-Backend/Labelix-AI-Backend/Controllers/AIConfigController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Labelix_AI_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AIConfigController : ControllerBase
+    {
+
+        private readonly ILogger<AIConfigController> _logger;
+
+        public AIConfigController(ILogger<AIConfigController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPut]
+        public void Put([FromBody] string j)
+        {
+        }
+
+
+
+    }
+}
