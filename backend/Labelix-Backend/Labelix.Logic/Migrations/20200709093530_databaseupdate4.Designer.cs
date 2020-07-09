ad85c00d7df7c7972186fea9e0b857bed127a02d
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-09 11:39:13 +0200

    Updated Database

--- backend/Labelix-Backend/Labelix.Logic/Migrations/20200709093530_databaseupdate4.Designer.cs
+++ backend/Labelix-Backend/Labelix.Logic/Migrations/20200709093530_databaseupdate4.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Labelix.Logic.DataContext.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Labelix.Logic.Migrations
 {
     [DbContext(typeof(LabelixDbContext))]
-    partial class LabelixDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200709093530_databaseupdate4")]
+    partial class databaseupdate4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
