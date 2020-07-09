df467fa09ab611775b9b522161a804599da41f6d
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-09 14:44:29 +0200

    Add config-interfaces and Implementations

---
+++ ai-backend/Labelix-AI-Backend/Labelix-AI-Backend/Contracts/IAIConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labelix_AI_Backend.Contracts
+{
+    public interface IAIConfig : IIdentifiable, ICopyable<IAIConfig>
+    {
+        public string Name { get; set; }
+        public string DockerImageName { get; set; }
+        public string Parameter { get; set; }
+        public string InputDirectory { get; set; }
+        public string OutputDirectory { get; set; }
+    }
+}
