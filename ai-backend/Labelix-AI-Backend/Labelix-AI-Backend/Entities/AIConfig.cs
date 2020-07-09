6a43537d85f93be036a2a0f660a7e3aa81e4b986
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-09 14:44:29 +0200

    Add config-interfaces and Implementations

---
+++ ai-backend/Labelix-AI-Backend/Labelix-AI-Backend/Entities/AIConfig.cs
@@ -0,0 +1,26 @@
+﻿using Labelix_AI_Backend.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labelix_AI_Backend.Entities
+{
+    class AIConfig : IdentityObject, IAIConfig
+    {
+        public string Name { get; set; }
+        public string DockerImageName { get; set; }
+        public string Parameter { get; set; }
+        public string InputDirectory { get; set; }
+        public string OutputDirectory { get; set; }
+
+        public void CopyProperties(IAIConfig other)
+        {
+            Name = other.Name;
+            DockerImageName = other.DockerImageName;
+            Parameter = other.Parameter;
+            InputDirectory = other.InputDirectory;
+            OutputDirectory = other.OutputDirectory;
+        }
+    }
+}
