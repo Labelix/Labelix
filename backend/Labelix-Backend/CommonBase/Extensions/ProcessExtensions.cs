e3bc0a1f5752e1de587bb854f6b6771ae62cf966
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-13 16:46:53 +0200

    Remove unnecessesary parameters from ProcessHelper/ProcessExtension methods

--- backend/Labelix-Backend/CommonBase/Extensions/ProcessExtensions.cs
+++ backend/Labelix-Backend/CommonBase/Extensions/ProcessExtensions.cs
@@ -12,11 +12,9 @@ namespace CommonBase.Extensions
         /// Extension method to run a process asyncronously.
         /// </summary>
         /// <param name="process">This Process.</param>
-        /// <param name="noWindow">Defines whether the application should be started in windowed mode.</param>
-        /// <param name="shellExecute">Defines whether the application is run by the systems shell or the process is started directly.</param>
         /// <returns>The processes exit code.</returns>
-        public static Task<int> RunProcessAsync(this Process process, bool noWindow = true, bool shellExecute = false) {
-            return RunProcessAsync(process, noWindow, shellExecute);
+        public static Task<int> RunProcessAsync(this Process process) {
+            return RunProcessAsync(process);
         }
 
         /// <summary>
@@ -26,9 +24,9 @@ namespace CommonBase.Extensions
         /// <param name="noWindow">Defines whether the application should be started in windowed mode.</param>
         /// <param name="shellExecute">Defines whether the application is run by the systems shell or the process is started directly.</param>
         /// <returns>The processes exit code.</returns>
-        public static int RunProcess(string fileName, string arguments, bool noWindow = true, bool shellExecute = false)
+        public static int RunProcess(this Process process)
         {
-            return RunProcess(fileName, arguments, noWindow, shellExecute);
+            return RunProcess(process);
         }
     }
 }
