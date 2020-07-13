538b5949a534e99616e52343beca46b46ed542fb
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-13 10:39:19 +0200

    Test to invoke docker command ba command line

--- backend/Labelix-Backend/Labelix.AIBackend/Controllers/AIConfigController.cs
+++ backend/Labelix-Backend/Labelix.AIBackend/Controllers/AIConfigController.cs
@@ -25,6 +25,28 @@ namespace Labelix.AIBackend.Controllers
         {
             System.Diagnostics.Debug.WriteLine(config);
 
+            var processInfo = new ProcessStartInfo("docker.exe", $"run -it --rm hello-world");
+
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            //processInfo.RedirectStandardOutput = true;
+            //processInfo.RedirectStandardError = true;
+
+            int exitCode;
+            using var process = new Process();
+
+                process.StartInfo = processInfo;
+                // process.OutputDataReceived += new DataReceivedEventHandler(logOrWhatever());
+                // process.ErrorDataReceived += new DataReceivedEventHandler(logOrWhatever());
+
+                process.Start();
+
+                process.WaitForExit();
+            exitCode = process.ExitCode;
+            
+
+
+
             return config;
         }
 
