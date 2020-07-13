8e0a1038a633dd663693458cd37d269c5ac3ee5b
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-13 10:44:06 +0200

    Enambled Image safing by name and format

--- backend/Labelix-Backend/Labelix.WebAPI/Controllers/Base64Controller.cs
+++ backend/Labelix-Backend/Labelix.WebAPI/Controllers/Base64Controller.cs
@@ -18,9 +18,17 @@ namespace Labelix.WebAPI.Controllers
         [HttpPost("UploadImage")]
         public HttpResponseMessage ImageUpload(Data data)
         {
-            var bytes = ImageExtensions.Base64ToByte(data.Base64);
-            System.IO.File.WriteAllBytes("./Resources/Images/image.png", bytes);
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            try
+            {
+                var bytes = ImageExtensions.Base64ToByte(data.Base64);
+                System.IO.File.WriteAllBytes($"./Resources/Images/{data.Name}.{data.Format}", bytes);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch(Exception er)
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+            
         }
     }
 
