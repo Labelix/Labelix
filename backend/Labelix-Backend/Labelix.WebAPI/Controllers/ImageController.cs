542bbd9a6501d0601153a156abd555de116eece6
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-13 17:59:25 +0200

    Added Image Download

--- backend/Labelix-Backend/Labelix.WebAPI/Controllers/ImageController.cs
+++ backend/Labelix-Backend/Labelix.WebAPI/Controllers/ImageController.cs
@@ -1,4 +1,6 @@
-﻿using Labelix.WebApi.Controllers;
+﻿using CommonBase.Extensions;
+using Labelix.Transfer.Modules;
+using Labelix.WebApi.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -11,11 +13,29 @@ namespace Labelix.WebAPI.Controllers
     public class ImageController : GenericController<Contract, Model>
     {
         [HttpGet("{id}")]
-        public Task<Model> GetAsync(int id)
+        public async Task<Data> GetAsync(int id)
         {
-            return GetModelByIdAsync(id);
+            Model image = await GetModelByIdAsync(id);
+			byte[] bytes = System.IO.File.ReadAllBytes(image.ImagePath);
+			string base64 = bytes.ImageToBase64();
+			Data data = new Data();
+			data.Base64 = base64;
+			string[] pathParts = image.ImagePath.Split('/');
+			data.Name = pathParts[pathParts.Length - 1];
+			data.Format = data.Name.Split('.')[1];
+			data.ProjectId = image.ProjectId;
+
+			return GetXMLOfBase(data);
         }
-        [HttpGet("all")]
+
+		//Sets the base64 to xml variant
+		private Data GetXMLOfBase(Data data)
+		{
+			data.Base64 = $"data:image/{data.Format};base64,{data.Base64}";
+			return data;
+		}
+
+		[HttpGet("all")]
         public Task<IEnumerable<Model>> GetAllAsync()
         {
             return GetModelsAsync();
