f07f3a132ef9fed3324aa1a9b564a43224713057
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-13 10:06:42 +0200

    Add AIConfigController, Delete Weather forecast controller

---
+++ backend/Labelix-Backend/Labelix.AIBackend/Controllers/AIConfigController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Labelix.Transfer.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labelix.AIBackend.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AIConfigController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return $"Controller works!";
+        }
+
+        [HttpPost]
+        public AIConfig Post([System.Web.Http.FromBody] AIConfig config)
+        {
+            System.Diagnostics.Debug.WriteLine(config);
+
+            return config;
+        }
+
+    }
+}
