b1dc0222268571418aec826425623492fb7e510a
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-13 15:27:56 +0200

    Add ProcessHelper to CommonBase

---
+++ backend/Labelix-Backend/CommonBase/Helpers/ProcessHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonBase.Helpers
+{
+    public static partial class ProcessHelper
+    {
+        public static Task<int> RunProcessAsync(string fileName, string arguments, bool noWindow = true, bool shellExecute = false)
+        {
+
+            using var process = new Process
+            {
+                StartInfo = {
+                    FileName = fileName,
+                    Arguments = arguments,
+                    CreateNoWindow = noWindow,
+                    UseShellExecute = shellExecute
+                }
+            };
+
+            return RunProcessAsync(process, noWindow, shellExecute);
+        }
+
+        public static int RunProcess(string fileName, string arguments, bool noWindow = true, bool shellExecute = false)
+        {
+            return RunProcessAsync(fileName, arguments, noWindow, shellExecute).Result;
+        }
+
+        public static Task<int> RunProcessAsync(Process process, bool noWindow = true, bool shellExecute = false)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) =>
+            {
+                tcs.SetResult(process.ExitCode);
+                process.Dispose();
+            };
+
+            return tcs.Task;
+        }
+
+        public static int RunProcess(Process process, string arguments, bool noWindow = true, bool shellExecute = false)
+        {
+            return RunProcessAsync(process, noWindow, shellExecute).Result;
+        }
+    }
+}
--- backend/Labelix-Backend/Labelix.AIBackend/Controllers/AIConfigController.cs
+++ backend/Labelix-Backend/Labelix.AIBackend/Controllers/AIConfigController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Labelix.DockerUtils;
 using Labelix.Transfer.Persistence;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,32 +27,7 @@ namespace Labelix.AIBackend.Controllers
         [HttpPost]
         public int Post([System.Web.Http.FromBody] AIConfig config)
         {
-            //var exists = File.Exists(PATH);
-            //Debug.WriteLine($"File found: {exists}");
-
-            var processInfo = new ProcessStartInfo(@"C:/Program Files/Docker/Docker/resources/bin/docker.exe", $"run -it --rm {config.DockerImageName} {config.Parameter}");
-
-            processInfo.CreateNoWindow = true;
-            processInfo.UseShellExecute = false;
-            //processInfo.RedirectStandardOutput = true;
-            //processInfo.RedirectStandardError = true;
-
-            int exitCode;
-            using var process = new Process();
-
-            process.StartInfo = processInfo;
-            //process.OutputDataReceived += new DataReceivedEventHandler(logOrWhatever());
-            //process.ErrorDataReceived += new DataReceivedEventHandler(logOrWhatever());
-
-            process.Start();
-
-            process.WaitForExit();
-            exitCode = process.ExitCode;
-
-
-
-
-            return exitCode;
+            return DockerUtils.DockerUtils.DockerRun(config.DockerImageName, config.Parameter);
         }
 
     }
