4ddc6afebf9f0d1facff2b29ea1b65922073d343
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-13 09:38:09 +0200

    Fixed Image Upload

--- backend/Labelix-Backend/Labelix.WebAPI/Controllers/Base64Controller.cs
+++ backend/Labelix-Backend/Labelix.WebAPI/Controllers/Base64Controller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using CommonBase.Extensions;
 using Labelix.WebAPI.Modules;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,7 +18,11 @@ namespace Labelix.WebAPI.Controllers
         [HttpPost("UploadImage")]
         public HttpResponseMessage ImageUpload(Data data)
         {
+            var bytes = ImageExtensions.Base64ToByte(data.Base64);
+            System.IO.File.WriteAllBytes("./Resources/Images/image.png", bytes);
             return new HttpResponseMessage(HttpStatusCode.OK);
         }
     }
+
+
 }
