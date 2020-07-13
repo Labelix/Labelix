68c920c79d3f062edbf8c6e7369908c0b20b04e7
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-13 14:22:29 +0200

    Update launchSettings.json

--- backend/Labelix-Backend/Labelix.AIBackend/Program.cs
+++ backend/Labelix-Backend/Labelix.AIBackend/Program.cs
@@ -13,7 +13,7 @@ namespace Labelix.AIBackend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
