19bf052e6f3615cff9dd1c885192d8bae54c3d3f
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-13 14:45:58 +0200

    Add DockerUtils Class library

---
+++ backend/Labelix-Backend/DockerUtils/DockerUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace Labelix.DockerUtils
+{
+    public static class DockerUtils
+    {
+        public static int DockerRun() {
+            return 0
+        }
+    }
+}
