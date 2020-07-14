053e9410e1b4ad9ecd2f6b5ebb7d9181bc21208f
Christoph Knoll <c.knoll@htblaleonding.onmicrosoft.com>
2020-07-14 10:30:08 +0200

    Add enum exit codes

--- backend/Labelix-Backend/DockerUtils/ErrorCodes.cs
+++ backend/Labelix-Backend/DockerUtils/ErrorCodes.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace DockerUtils
 {
-    class ErrorCodes
+    enum ErrorCodes
     {
+        Success = 0,
+        DockerRunFailed = 125,
+        ContainedCommandNotInvoked = 126,
+        ContainedCommandNotFound = 127,
+        FatalError = 128,
+        TerminatedByCTRLC = 130,
+        SIGKILL = 137,
+        SIGTERM = 143
     }
 }
