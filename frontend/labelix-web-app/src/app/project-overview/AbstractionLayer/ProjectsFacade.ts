4ac4fbf66c9df2ab4c2107c91e724d6831af1218
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-20 15:38:46 +0200

    Refactored ProjectFacade

--- frontend/labelix-web-app/src/app/project-overview/AbstractionLayer/ProjectsFacade.ts
+++ frontend/labelix-web-app/src/app/project-overview/AbstractionLayer/ProjectsFacade.ts
@@ -1,18 +1,20 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import { ProjectServiceService } from '../CoreLayer/services/project-service.service';
+import {select, Store} from '@ngrx/store';
+import {getAllProjects, getNumberOfExistingProjects, ProjectState} from '../CoreLayer/states/projectState';
 import {IProject} from '../../utility/contracts/IProject';
-import {ProjectServiceService} from '../CoreLayer/services/project-service.service';
+import {AddProjectAction, DeleteProjectAction} from '../CoreLayer/actions/project.actions';
 import {shareReplay} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class ProjectsFacade {
-  private projects$: Observable<IProject[]>;
+  projects$: Observable<IProject[]>;
+  numberOfProjects$: Observable<number>;
 
-  constructor(private projectApi: ProjectServiceService) {
-    this.projects$ = this.projectApi.getItems()
-      .pipe(
-        shareReplay(1)
-      );
+  constructor(private projectApi: ProjectServiceService, private store: Store<ProjectState>) {
+    this.projects$ = this.store.pipe(select(getAllProjects));
+    this.numberOfProjects$ = this.store.pipe(select(getNumberOfExistingProjects));
   }
 
   getProjects() {
