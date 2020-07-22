31a242860b14a952217c2e5b94043728ea4b2f37
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-07-22 09:50:25 +0200

    Added IAIModelConfig

---
+++ frontend/labelix-web-app/src/app/utility/contracts/IAIModelConfig.ts
@@ -0,0 +1,8 @@
+export interface IAIModelConfig extends IIdentifiable{
+  name: string;
+  dockerImageName: string;
+  parameter: string;
+  inputDirectory: string;
+  outputDirectory: string;
+  options: string;
+}
