f824905a666e560da68515bd0734cf0508be5d7a
florentin <florentingewessler@gmail.com>
2020-09-02 09:58:48 +0200

    make annotation part more responsive

--- frontend/labelix-web-app/src/app/project-overview/CoreLayer/services/image-service.service.ts
+++ frontend/labelix-web-app/src/app/project-overview/CoreLayer/services/image-service.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {GenericApiService} from '../../../utility/logic/generic-api.service';
+import {IImage} from '../../../utility/contracts/IImage';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ImageServiceService {
+export class ImageServiceService extends GenericApiService<IImage> {
 
-  constructor() { }
+  constructor(protected  httpClient: HttpClient) {
+    super(httpClient);
+    this.urlRoot = 'api/image';
+  }
 }
--- frontend/labelix-web-app/src/app/utility/contracts/IImage.ts
+++ frontend/labelix-web-app/src/app/utility/contracts/IImage.ts
@@ -1,6 +1,7 @@
 import {IIdentifiable} from './IIdentifiable';
 
 export interface IImage extends IIdentifiable{
-  imagePath: string;
-  labeledPath: string;
+  imageId: number;
+  projectId: number;
+  base64: string;
 }
