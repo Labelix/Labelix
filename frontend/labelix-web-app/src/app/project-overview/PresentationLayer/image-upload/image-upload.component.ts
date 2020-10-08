a82bc1c5f2857c28238f24f7a2ec4538d740dfd0
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-10-08 20:57:52 +0200

    Image Upload in Project Creation

--- frontend/labelix-web-app/src/app/project-overview/PresentationLayer/image-upload/image-upload.component.ts
+++ frontend/labelix-web-app/src/app/project-overview/PresentationLayer/image-upload/image-upload.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {IRawImage} from '../../../utility/contracts/IRawImage';
+import {IImage} from '../../../utility/contracts/IImage';
 
 @Component({
   selector: 'app-image-upload',
@@ -12,4 +14,30 @@ export class ImageUploadComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onFileDropped($event: any) {
+    const tmp: IRawImage[] = [];
+
+    let count = 1;
+    for (const item of $event) {
+      // base 64 encoding wird später hinzugefügt
+      tmp.push({id: count, file: item, height: -1, width: -1, base64Url: '', name: item.name});
+      count++;
+    }
+    const reader = new FileReader();
+    const image = new Image();
+    reader.addEventListener('load', (event: any) => {
+      image.src = event.target.result;
+      image.onload = () => {
+        const newRawImage = {
+          id: 0,
+          file: tmp[0].file,
+          width: image.width,
+          height: image.height,
+          base64Url: image.src,
+          name: tmp[0].file.name
+        };
+      };
+    });
+    reader.readAsDataURL(tmp[0].file);
+  }
 }
