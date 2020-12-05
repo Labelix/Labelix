3696c2542ecd817efaa8e182554eda1cb64026b0
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-12-05 16:38:58 +0100

    IAILabelInfo now implements IIdentifyable

--- frontend/labelix-web-app/src/app/utility/contracts/IAILabelInfo.ts
+++ frontend/labelix-web-app/src/app/utility/contracts/IAILabelInfo.ts
@@ -1,7 +1,8 @@
 import {IImage} from './IImage';
 import {IAIModelConfig} from './IAIModelConfig';
+import {IIdentifiable} from './IIdentifiable';
 
-export interface IAILabelInfo {
+export interface IAILabelInfo extends IIdentifiable{
   aiModelConfig: IAIModelConfig;
   images: IImage[];
 }
