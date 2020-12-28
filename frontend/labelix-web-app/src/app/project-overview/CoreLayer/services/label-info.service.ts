40f51b6f668a0618e04c4193988ba7cff0b6bdbe
florentin <florentingewessler@gmail.com>
2020-12-28 19:31:58 +0100

    Add method to label info service

--- frontend/labelix-web-app/src/app/project-overview/CoreLayer/services/label-info.service.ts
+++ frontend/labelix-web-app/src/app/project-overview/CoreLayer/services/label-info.service.ts
@@ -3,6 +3,7 @@ import {GenericApiService} from '../../../utility/logic/generic-api.service';
 import {IAILabelInfo} from '../../../utility/contracts/IAILabelInfo';
 import {HttpClient} from '@angular/common/http';
 import {OAuthService} from 'angular-oauth2-oidc';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class LabelInfoService extends GenericApiService<IAILabelInfo> {
   constructor(protected  httpClient: HttpClient, protected oauthService: OAuthService) {
     super(httpClient, oauthService);
     this.urlRoot = 'AIConfig';
+  }
+
+  // dadurch, dass der Rückgabewert keine IAILabelInfo ist, kann der GenericApiService in diesem Fall nicht benutzt werden
+  postLabelInfo(item: IAILabelInfo): Observable<any> {
+    return this.httpClient.put(this.urlRoot, item);
   }
 }
