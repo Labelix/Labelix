859878c980eea31921c660c3c9b8c59f283a93d5
florentin <florentingewessler@gmail.com>
2020-12-28 19:30:53 +0100

    alter IImage.ts to include width and height of image

--- frontend/labelix-web-app/src/app/utility/contracts/IImage.ts
+++ frontend/labelix-web-app/src/app/utility/contracts/IImage.ts
@@ -1,9 +1,14 @@
 import {IIdentifiable} from './IIdentifiable';
 
-export interface IImage extends IIdentifiable{
+export interface IImage extends IIdentifiable {
+  // is the base64 string
   Data: string;
   imageId: number;
   projectId: number;
   format: string;
   name: string;
+  // die Höhe und die Breite des Bildes braucht man hier, um die Response vom AI Backend richtig
+  // auszulesen ansonsten kann man diese werte ignorieren und bzw auf -1 stellen
+  width: number;
+  height: number;
 }
