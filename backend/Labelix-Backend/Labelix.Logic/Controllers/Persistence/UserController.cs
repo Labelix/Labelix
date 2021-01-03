994b206f43aa8d41f81c3c78f5407a0f47985a64
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-03 19:23:59 +0100

    Added UserController

---
+++ backend/Labelix-Backend/Labelix.Logic/Controllers/Persistence/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Labelix.Contracts.Persistence;
+using Labelix.Logic.DataContext;
+using Labelix.Logic.Entities.Persistence;
+
+namespace Labelix.Logic.Controllers.Persistence
+{
+    class UserController : GenericController<IUser, User>
+    {
+        public UserController(IContext context) : base(context)
+        {
+        }
+
+        public UserController(ControllerObject controllerObject) : base(controllerObject)
+        {
+        }
+    }
+}
