f58d745e9e62657d8ef38004b73e226f2530b537
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-03 18:02:57 +0100

    Added Authorization to Backend

--- backend/Labelix-Backend/Labelix.WebAPI/Startup.cs
+++ backend/Labelix-Backend/Labelix.WebAPI/Startup.cs
@@ -1,41 +1,74 @@
-using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace Labelix.WebAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IHostEnvironment env)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
-			Enviroment = env;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
-        public IHostEnvironment Enviroment { get; }
+        public IWebHostEnvironment Environment { get; }
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-
-            services.Configure<FormOptions>(o =>
-            {
-                o.ValueLengthLimit = int.MaxValue;
-                o.MultipartBodyLengthLimit = int.MaxValue;
-                o.MemoryBufferThreshold = int.MaxValue;
-            });
-
             services.AddControllers();
+            
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(o =>
+            {
+                o.Authority = Configuration["Jwt:Authority"];
+                o.Audience = Configuration["Jwt:Audience"];
+                o.RequireHttpsMetadata = false;
+
+                o.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        c.NoResult();
+
+                        c.Response.StatusCode = 500;
+                        c.Response.ContentType = "text/plain";
+
+                        if (Environment.IsDevelopment())
+                        {
+                            return c.Response.WriteAsync(c.Exception.ToString());
+                        }
+
+                        return c.Response.WriteAsync("An error occured processing your authentication.");
+                    }
+                };
+
+                o.TokenValidationParameters = new
+                    TokenValidationParameters()
+                    {
+                        ValidateAudience = false
+                    };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,12 +81,6 @@ namespace Labelix.WebAPI
 
             app.UseHttpsRedirection();
 
-            app.UseStaticFiles();
-            //app.UseStaticFiles(new StaticFileOptions
-            //{
-            //    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),@"Resources")),
-            //    RequestPath = new Microsoft.AspNetCore.Http.PathString("/Resources")
-            //});
 
             app.UseRouting();
             app.UseAuthentication();
@@ -64,6 +91,8 @@ namespace Labelix.WebAPI
             {
                 endpoints.MapControllers();
             });
+            app.UseResponseCaching();
+            app.UseStaticFiles();
         }
     }
 }
