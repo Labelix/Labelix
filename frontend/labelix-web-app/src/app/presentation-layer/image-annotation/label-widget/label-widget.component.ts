16c7c0eebc4c77c029519a506a6d7721178efa41
florentin <florentingewessler@gmail.com>
2021-01-04 18:15:51 +0100

    Revert architecture

---
+++ frontend/labelix-web-app/src/app/presentation-layer/image-annotation/label-widget/label-widget.component.ts
@@ -0,0 +1,61 @@
+import {Component, OnInit} from '@angular/core';
+import {LabelCategoryFacade} from '../../../abstraction-layer/LabelCategoryFacade';
+import {ICategory} from '../../../core-layer/utility/contracts/ICategory';
+import {AnnotationFacade} from '../../../abstraction-layer/AnnotationFacade';
+import {AnnotaionMode} from '../../../core-layer/annotaionModeEnum';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {ImageAnnotationHelper} from '../../../core-layer/helper/image-annotation-helper';
+import {SingleAnnotationExportFormComponent} from '../single-annotation-export-form/single-annotation-export-form.component';
+import {LabelSettingsDialogComponent} from '../label-settings-dialog/label-settings-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-label-widget',
+  templateUrl: './label-widget.component.html',
+  styleUrls: ['./label-widget.component.css']
+})
+export class LabelWidgetComponent implements OnInit {
+
+  currentLabelCategories: ICategory[] = [];
+  currentAnnotationMode: AnnotaionMode;
+  selectedCategoryLabel: ICategory;
+
+  currentlyAdding = false;
+
+  newLabelName: string;
+  newSupercategory: string;
+  numExistingLabels: number;
+
+  nextLabelId: number;
+
+  constructor(private facade: LabelCategoryFacade,
+              private annotationFacade: AnnotationFacade,
+              public dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {
+    this.facade.labelCategories$.subscribe(value => this.currentLabelCategories = value);
+    this.annotationFacade.currentAnnotationMode.subscribe(value => this.currentAnnotationMode = value);
+    this.facade.numberOfExistingLabels$.subscribe(value => this.numExistingLabels = value);
+    this.annotationFacade.activeLabel.subscribe(value => this.selectedCategoryLabel = value);
+    this.facade.nextLabelId$.subscribe(value => this.nextLabelId = value);
+  }
+
+  onAddLabel() {
+    this.currentlyAdding = true;
+  }
+
+  onSettings() {
+    this.dialog.open(LabelSettingsDialogComponent);
+  }
+
+  onLabelClick(item: ICategory) {
+    this.annotationFacade.changeActiveLabel(item);
+    // wenn nur das ganze Bild annotiert werden soll, kann sofort eine Annotierung für das gesamte Bild gespeichert
+    // werden
+    if (this.currentAnnotationMode === AnnotaionMode.WHOLE_IMAGE) {
+      this.annotationFacade.addWholeImageAnnotation(this.selectedCategoryLabel);
+    }
+  }
+
+}
---
+++ frontend/labelix-web-app/src/app/presentation-layer/image-annotation/single-annotation-export-form/single-annotation-export-form.component.ts
@@ -0,0 +1,61 @@
+import {Component, OnInit} from '@angular/core';
+import {CocoFormatController} from '../../../core-layer/controller/CocoFormatController';
+import {AnnotationFacade} from '../../../abstraction-layer/AnnotationFacade';
+import {RawImageFacade} from '../../../abstraction-layer/RawImageFacade';
+import {LabelCategoryFacade} from '../../../abstraction-layer/LabelCategoryFacade';
+import {ICategory} from '../../../core-layer/utility/contracts/ICategory';
+import {IImageAnnotation} from '../../../core-layer/utility/contracts/IImageAnnotation';
+import {IRawImage} from '../../../core-layer/utility/contracts/IRawImage';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-single-annotation-export-form',
+  templateUrl: './single-annotation-export-form.component.html',
+  styleUrls: ['./single-annotation-export-form.component.css']
+})
+export class SingleAnnotationExportFormComponent implements OnInit {
+
+  constructor(private annotationFacade: AnnotationFacade,
+              private rawImageFacade: RawImageFacade,
+              private labelCategoryFacade: LabelCategoryFacade,
+              private sanitizer: DomSanitizer,
+              private cocoFormatter: CocoFormatController) {
+  }
+
+  description: string = undefined;
+  contributor: string = undefined;
+  inputUrl: string = undefined;
+
+  url: SafeUrl = undefined;
+
+  currentCategoryLabels: ICategory[];
+  currentImageAnnotations: IImageAnnotation[];
+  currentRawImages: IRawImage[];
+
+  ngOnInit(): void {
+    this.annotationFacade.currentImageAnnotations.subscribe(value => this.currentImageAnnotations = value);
+    this.rawImageFacade.files$.subscribe(value => this.currentRawImages = value);
+    this.labelCategoryFacade.labelCategories$.subscribe(value => this.currentCategoryLabels = value);
+  }
+
+  onExport() {
+    const jsonObject = JSON.stringify({
+      categories: this.cocoFormatter.createListOfICocoCategory(this.currentCategoryLabels),
+      annotations: this.cocoFormatter.getCocoAnnotations(this.currentImageAnnotations),
+      licenses: [this.cocoFormatter.getTestLicense()],
+      images: this.cocoFormatter.createListOfICocoImages(this.currentRawImages),
+      info: {
+        year: 2020,
+        description: this.description,
+        version: '1.0',
+        url: this.inputUrl,
+        dateCreated: new Date(Date.now()),
+        contributor: this.contributor
+      }
+    });
+
+    this.url = this.sanitizer.bypassSecurityTrustUrl('data:text/json;charset=UTF-8,' + jsonObject);
+  }
+
+}
