f8cb376b7062f962b136dee56a9c3e6511f2f6be
Lukas Gaisbauer <gaisbauer.lukas@gmx.at>
2021-01-05 16:16:26 +0100

    Added Frontend From Developement

---
+++ frontend/labelix-web-app/src/app/presentation-layer/project-overview/image-timeline/image-timeline.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {IRawImage} from '../../../core-layer/utility/contracts/IRawImage';
+import {ProjectImageUploadFacade} from '../../../abstraction-layer/ProjectImageUploadFacade';
+
+@Component({
+  selector: 'app-image-timeline',
+  templateUrl: './image-timeline.component.html',
+  styleUrls: ['./image-timeline.component.css']
+})
+export class ImageTimelineComponent implements OnInit {
+  listOfRawImages: IRawImage[];
+
+  constructor(private rawImageFacade: ProjectImageUploadFacade) {
+  }
+  ngOnInit(): void {
+    this.rawImageFacade.rawImages$.subscribe(value => {
+      this.listOfRawImages = value;
+      for (const item of this.listOfRawImages) {
+        if (item.base64Url === undefined) {
+          this.getBase64(item);
+        }
+      }
+    });
+  }
+
+  private getBase64(item: IRawImage) {
+    const reader = new FileReader();
+    reader.addEventListener('load', (event: any) => {
+      this.rawImageFacade.addBase64CodeToIFile({
+        id: item.id,
+        baseCode: event.target.result
+      });
+    });
+    reader.readAsDataURL(item.file);
+  }
+}
