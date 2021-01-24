28f1e0f9a9d96261afac8db13711afe7df220a44
florentin <florentingewessler@gmail.com>
2021-01-24 11:11:11 +0100

    Fix login

--- frontend/labelix-web-app/src/app/abstraction-layer/UserFacade.ts
+++ frontend/labelix-web-app/src/app/abstraction-layer/UserFacade.ts
@@ -1,11 +1,12 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {IUser} from '../core-layer/contracts/IUser';
 import {UserService} from '../core-layer/services/user.service';
 import {getAllUsers, UserState} from '../core-layer/states/state-definitions/userState';
 import {select, Store} from '@ngrx/store';
 import {AddUsers, ClearUsers} from '../core-layer/states/actions/user.actions';
 import {OAuthService} from 'angular-oauth2-oidc';
+import {tokenReference} from '@angular/compiler';
 
 
 // is responsible for everything which has to do with users, this involves authentication and management of other users
@@ -13,19 +14,17 @@ import {OAuthService} from 'angular-oauth2-oidc';
 export class UserFacade {
 
   users$: Observable<IUser[]>;
+  isLoggedIn$: Subject<boolean>;
 
   constructor(private userApi: UserService,
               private oauthService: OAuthService,
               private store: Store<UserState>) {
     this.users$ = store.pipe(select(getAllUsers));
+    this.isLoggedIn$ = new Subject<boolean>();
   }
 
   getIdentityClaims(): object {
     return this.oauthService.getIdentityClaims();
-  }
-
-  isLoggedIn(): boolean {
-    return this.oauthService.hasValidAccessToken();
   }
 
   isAdmin(): boolean {
@@ -33,20 +32,30 @@ export class UserFacade {
     return this.oauthService.getIdentityClaims().roles.indexOf('admin') !== -1;
   }
 
+  checkLoggedIn(): boolean {
+    const hasIdToken = this.oauthService.hasValidIdToken();
+    const hasAccessToken = this.oauthService.hasValidAccessToken();
+
+    return (hasIdToken && hasAccessToken);
+  }
+
   login() {
-    this.oauthService.tryLogin()
-      .catch(err => {
-        console.error(err);
-      })
-      .then(() => {
-        if (!this.oauthService.hasValidAccessToken()) {
-          this.oauthService.initImplicitFlow();
-        }
-      });
+  this.oauthService.tryLogin()
+    .catch(err => {
+      console.error(err);
+    })
+    .then(() => {
+      if (!this.oauthService.hasValidAccessToken()) {
+        this.oauthService.initImplicitFlow();
+      } else {
+        this.isLoggedIn$.next(true);
+      }
+    });
   }
 
   logout() {
     this.oauthService.logOut();
+    this.isLoggedIn$.next(false);
   }
 
   getUsers() {
