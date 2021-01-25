5cbfb9e2151b2478b6aec95c290ef77dd5008635
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-25 21:01:42 +0100

    Added Width and Height to IImage

--- backend/Labelix-Backend/Labelix.Contracts/Persistence/IImage.cs
+++ backend/Labelix-Backend/Labelix.Contracts/Persistence/IImage.cs
@@ -3,6 +3,8 @@
     public interface IImage : IIdentifiable, ICopyable<IImage>
     {
         string ImagePath { get; set; }
+        double Height { get; set; }
+        double Width { get; set; }
         int ProjectId { get; set; }
     }
 }
