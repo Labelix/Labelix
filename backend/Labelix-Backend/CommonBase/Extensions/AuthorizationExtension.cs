2f0a3da2173301ec99465c45139844911fecbd9f
florentin <florentingewessler@gmail.com>
2021-02-03 10:51:53 +0100

    Change dictionary key for fetching the username from the token

--- backend/Labelix-Backend/CommonBase/Extensions/AuthorizationExtension.cs
+++ backend/Labelix-Backend/CommonBase/Extensions/AuthorizationExtension.cs
@@ -10,7 +10,7 @@ namespace CommonBase.Extensions
     {
         public static string GetUserId(this IEnumerable<Claim> claims)
         {
-            return claims.First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").Value;
+            return claims.First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname").Value;
         }
     }
 }
