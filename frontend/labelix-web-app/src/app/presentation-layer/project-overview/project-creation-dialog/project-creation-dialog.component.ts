4dc4206d0398e99b2c39228f14cc595252cd4e09
florentin <florentingewessler@gmail.com>
2021-02-06 13:04:59 +0100

    Add name check to project creation and editing

--- frontend/labelix-web-app/src/app/presentation-layer/project-overview/project-creation-dialog/project-creation-dialog.component.ts
+++ frontend/labelix-web-app/src/app/presentation-layer/project-overview/project-creation-dialog/project-creation-dialog.component.ts
@@ -40,6 +40,8 @@ export class ProjectCreationDialogComponent implements OnInit, OnDestroy {
   filteredUsers: IUser[];
   selectedUsers: IUser[] = [];
 
+  allProjects: IProject[] = [];
+
   constructor(public dialogRef: MatDialogRef<ProjectCreationDialogComponent>,
               private snackBar: MatSnackBar,
               private projectFacade: ProjectsFacade,
@@ -56,6 +58,7 @@ export class ProjectCreationDialogComponent implements OnInit, OnDestroy {
     this.aiModelConfigFacade.loadAllConfigsToState();
     this.userFacade.loadUsersIntoState();
 
+    this.subscription.add(this.projectFacade.projects$.subscribe(value => this.allProjects = value));
     this.subscription.add(this.rawImageFacade.rawImages$.subscribe((m) => this.images = m));
     this.subscription.add(this.aiModelConfigFacade.aiModelConfigs$.subscribe((value) => this.allAiConfigs = value));
     this.subscription.add(this.userFacade.users$.subscribe((value) => this.allUsers = value));
@@ -160,6 +163,11 @@ export class ProjectCreationDialogComponent implements OnInit, OnDestroy {
       return false;
     } else if (this.images.length === 0) {
       this.snackBar.open('Please add pictures to the project', '', {
+        duration: 2000,
+      });
+      return false;
+    } else if (this.projectFacade.checkIfNameIsAlreadyPresent(this.newProjectName, this.allProjects)){
+      this.snackBar.open('A project with this name is already present', '', {
         duration: 2000,
       });
       return false;
